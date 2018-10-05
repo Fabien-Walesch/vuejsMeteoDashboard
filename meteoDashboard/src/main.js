73aed803ec7b975f42f3cf6b3bc2cb59c8e561fc
Fabien Walesch <fabien.walesch@gmail.com>
2018-10-05 13:44:17 +0200

    router created

--- meteoDashboard/src/main.js
+++ meteoDashboard/src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [{
+    path: '/',
+    components: require ('./homePage/homePage.vue')
+  }]
+})
+
 import App from './App.vue'
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
